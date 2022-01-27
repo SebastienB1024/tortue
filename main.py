34a69aeb2b2af205e34ba579bde225e0c0355177
SebastienB1024 <78688903+SebastienB1024@users.noreply.github.com>
2022-01-27 18:07:46 +0100

    V0.5 Define module + show turtle

--- main.py
+++ main.py
@@ -3,8 +3,53 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 import turtle
 
-t = turtle.Turtle()
+def escalier(tortue,iteration,longueur):
+    tortue.forward(longueur)
+    for i in range(0, iteration):
+
+        tortue.left(90)
+        tortue.forward(longueur)
+        tortue.right(90)
+        tortue.forward(longueur)
+
+def carre(tortue,longueur):
+    #tortue.forward(longueur)
+    for i in range(0, 4):
+        tortue.forward(longueur)
+        tortue.left(90)
+
+def carres(tortue,longueur,nbcarre):
+    for i in range (1, nbcarre+1):
+        carre(tortue, longueur)
+        longueur += 10
+
+
+
+# t = turtle.Turtle()
+# t.speed(2)
+
+t1 = turtle.Turtle()
+t1.speed(10)
+
+# escalier(t,5,30)
+
+taille = 30
+
+for i in range (0,4):
+    carres(t1,taille,20)
+    t1.left(90)
+
+t1.forward(800)
+
+
+
+
+
+
+
+
+
+
 turtle.done()
